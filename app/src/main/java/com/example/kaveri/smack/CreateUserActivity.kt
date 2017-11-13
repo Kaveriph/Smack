35c22e23c3e2e2271877d2e565534e41501a1a94
Kaveri <kaveriphi915@gmail.com>
2017-11-13 22:57:11 +0530

    Included the create user screen and logic to navigate to this screen on clicking the SIGN UP HERE button

---
+++ app/src/main/java/com/example/kaveri/smack/CreateUserActivity.kt
@@ -0,0 +1,25 @@
+package com.example.kaveri.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateBgColor(view: View) {
+
+    }
+
+    fun onTapToGenerateAvatar(view:View) {
+
+    }
+
+    fun createUserClicked(view:View) {
+
+    }
+}
