8e6a4e148ca686892ee3356eb8ae1cbc785fb6a7
Kaveri <kaveriphi915@gmail.com>
2017-11-13 21:37:54 +0530

    Included the login screen and provided navigation from login button in navigation layout to LoginActivity

---
+++ app/src/main/java/com/example/kaveri/smack/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.example.kaveri.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun onLoginClicked(view: View) {
+
+    }
+
+    fun signUpHereClicked(view:View) {
+
+    }
+}
