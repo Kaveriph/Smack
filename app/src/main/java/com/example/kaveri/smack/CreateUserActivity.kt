a20c51d7d73e24007afde23f0363a93140f2463c
Kaveri <kaveriphi915@gmail.com>
2017-11-13 23:25:54 +0530

    Included the logic to generate the background color and avatar image for creating profile

--- app/src/main/java/com/example/kaveri/smack/CreateUserActivity.kt
+++ app/src/main/java/com/example/kaveri/smack/CreateUserActivity.kt
@@ -1,22 +1,49 @@
 package com.example.kaveri.smack
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
 
+    private var userAvatar= "profileDefault"
+    private var avatarColor = "[0.5, 0.5, 0.5, 1]"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
     }
 
     fun generateBgColor(view: View) {
+        val random = Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
 
+        signupUserAvatarImg.setBackgroundColor(Color.rgb(r,g,b))
+
+        val savedR = r/255
+        val savedG = g/255
+        val savedB = b/255
+
+        avatarColor = "[$savedR, $savedG, $savedB, 1]"
     }
 
     fun onTapToGenerateAvatar(view:View) {
 
+        val random = Random()
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
+
+        userAvatar = when(color) {
+            0 -> "light$avatar"
+            else -> "dark$avatar"
+        }
+
+        val resourceId = resources.getIdentifier(userAvatar,"drawable", this.packageName)
+        signupUserAvatarImg.setImageResource(resourceId)
     }
 
     fun createUserClicked(view:View) {
